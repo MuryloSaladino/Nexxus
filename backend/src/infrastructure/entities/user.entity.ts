455683086fe07ef72985d8e33c40b66eeb413852
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 09:08:44 -0300

    fix: incorrect attribute for user entity

--- backend/src/infrastructure/entities/user.entity.ts
+++ backend/src/infrastructure/entities/user.entity.ts
@@ -9,9 +9,6 @@ export class User extends BaseEntity implements UserModel {
     @Column({ type: "varchar", length: 20 })
     username: string;
 
-    @Column({ unique: true })
-    email: string;
-
     @Column({ select: false })
     password: string;
 
