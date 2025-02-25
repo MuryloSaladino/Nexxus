f1e0a31e62541301901d9b6290e87bfe2e0a2b58
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 13:28:53 -0300

    feat: login use cases

---
+++ backend/src/usecases/auth/login.usecases.ts
@@ -0,0 +1,28 @@
+import { IBcryptService } from "src/domain/adapters/bcrypt.interface";
+import { IJWTService } from "src/domain/adapters/jwt.interface";
+import { IUsersRepository } from "src/domain/repositories/user.repository";
+import { NotFoundError } from "src/infrastructure/errors/not-found.error";
+import { UnauthorizedError } from "src/infrastructure/errors/unauthrized.error";
+
+export class LoginUseCases {
+    constructor(
+        private readonly jwtService: IJWTService,
+        private readonly bcryptService: IBcryptService,
+        private readonly usersRepository: IUsersRepository,
+    ) {}
+
+    async login(username: string, password: string) {
+        const user = await this.usersRepository.findOneByUsername(username);
+        if(!user) throw new NotFoundError("Username not found");
+
+        const match = await this.bcryptService.compare(password, user.password);
+        if(!match) throw new UnauthorizedError("Invalid credentials");
+
+        const token = this.jwtService.createToken({
+            userId: user.id,
+            username: user.username,
+        });
+
+        return { token }
+    }
+}
