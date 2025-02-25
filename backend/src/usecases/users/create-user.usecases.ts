1f083a46cb8e8e98438bac1609e422103d2a85c0
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 02:39:31 -0300

    feat: create user use case

---
+++ backend/src/usecases/users/create-user.usecases.ts
@@ -0,0 +1,25 @@
+import { IBcryptService } from "src/domain/adapters/bcrypt.interface";
+import { UserModel } from "src/domain/models/user.model";
+import { IUsersRepository } from "src/domain/repositories/user.repository";
+import { ILogger } from "src/domain/services/logger.interface";
+import { BadRequestError } from "src/infrastructure/errors/bad-request.error";
+
+export class CreateUserUseCases {
+    constructor(
+        private readonly logger: ILogger,
+        private readonly usersRepository: IUsersRepository,
+        private readonly bcryptService: IBcryptService,
+    ) {}
+
+    async execute(username: string, password: string) {
+        const user = new UserModel();
+        user.username = username;
+        user.password = await this.bcryptService.hash(password);
+        
+        const result = await this.usersRepository.create(user);
+        
+        this.logger.log(`New user inserted to database { id: ${result.id} }`, "CreateUserUseCases");
+    
+        return result;
+    }
+}
