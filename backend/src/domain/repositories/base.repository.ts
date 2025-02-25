d1a14477a4bce58951688198e2f6a008043e55cf
MuryloSaladino <murylosaladino@gmail.com>
2025-02-24 21:52:53 -0300

    feat: base repository interface

---
+++ backend/src/domain/repositories/base.repository.ts
@@ -0,0 +1,9 @@
+import { BaseModel } from "../models/base.model";
+
+export interface IBaseRepository<T extends BaseModel> {
+    create(entity: Partial<T>): Promise<T>;
+    findById(id: string): Promise<T | null>;
+    findAll(): Promise<T[]>;
+    update(id: string, payload: Partial<T>): Promise<T | null>;
+    delete(id: string): Promise<void>;
+}
