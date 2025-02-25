3488bb5f9245cc251d11905b1ff4076dfaa1f621
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 10:14:10 -0300

    feat: update solution by id use case

---
+++ backend/src/usecases/solutions/update-solution.usecases.ts
@@ -0,0 +1,50 @@
+import { IBcryptService } from "src/domain/adapters/bcrypt.interface";
+import { ISolutionRepository } from "src/domain/repositories/solution.repository";
+import { IUsersRepository } from "src/domain/repositories/user.repository";
+import { ILogger } from "src/domain/services/logger.interface";
+import { Priority } from "src/domain/types/priority.type";
+import { Status } from "src/domain/types/status.type";
+import { NotFoundError } from "src/infrastructure/errors/not-found.error";
+
+interface ISolutionUpdate {
+    id: string;
+    userInChargeId?: string | null;
+    clientDepartment?: string;
+    benefit?: number | null;
+    investment?: number | null;
+    status?: Status;
+    priority?: Priority;
+    description?: string;
+    justification?: string;
+    orchestration?: string;
+}
+
+export class UpdateSolutionUseCases {
+    constructor(
+        private readonly solutionsRepository: ISolutionRepository,
+        private readonly usersRepository: IUsersRepository,
+    ) {}
+
+    async execute({
+        id,
+        userInChargeId,
+        ...payload
+    }: ISolutionUpdate) {
+        const solution = await this.solutionsRepository.findById(id);
+
+        if(!solution) {
+            throw new NotFoundError("User not found");
+        }
+        
+        if(userInChargeId) {
+            const user = await this.usersRepository.findById(userInChargeId);
+            if(!user) throw new NotFoundError("User to be in charge was not found");
+            solution.userInChargeId = userInChargeId;
+        }
+
+        return (await this.solutionsRepository.update(id, {
+            ...solution,
+            ...payload
+        }))!;
+    }
+}
