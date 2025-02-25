985b17b7d14e02012819dc582e01eec05e8eceb8
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 10:11:33 -0300

    feat: create solution use case

---
+++ backend/src/usecases/solutions/create-solution.usecases.ts
@@ -0,0 +1,55 @@
+import { SolutionModel } from "src/domain/models/solution.model";
+import { ISolutionRepository } from "src/domain/repositories/solution.repository";
+import { IUsersRepository } from "src/domain/repositories/user.repository";
+import { Priority } from "src/domain/types/priority.type";
+import { Status } from "src/domain/types/status.type";
+import { NotFoundError } from "src/infrastructure/errors/not-found.error";
+
+interface ISolutionCreation {
+    userInChargeId: string | null;
+    clientDepartment: string;
+    benefit: number | null;
+    investment: number | null;
+    status: Status;
+    priority: Priority;
+    description: string;
+    justification: string;
+    orchestration: string;
+}
+
+export class CreateSolutionUseCases {
+    constructor(
+        private readonly solutionsRepository: ISolutionRepository,
+        private readonly usersRepository: IUsersRepository,
+    ) {}
+
+    async execute({
+        userInChargeId,
+        clientDepartment,
+        benefit,
+        investment,
+        status,
+        priority,
+        description,
+        justification,
+        orchestration,
+    }: ISolutionCreation) {
+        if(userInChargeId) {
+            const user = await this.usersRepository.findById(userInChargeId);
+            if(!user) throw new NotFoundError("User to be in charge was not found");
+        }
+
+        const solution = new SolutionModel();
+        solution.userInChargeId = userInChargeId;
+        solution.clientDepartment = clientDepartment;
+        solution.benefit = benefit;
+        solution.investment = investment;
+        solution.status = status;
+        solution.priority = priority;
+        solution.description = description;
+        solution.justification = justification;
+        solution.orchestration = orchestration;
+
+        return await this.solutionsRepository.create(solution);
+    }
+}
