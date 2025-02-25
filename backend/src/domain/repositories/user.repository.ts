d0b2aecb2271b37624419b49e68cdf622a1bbc07
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 02:36:05 -0300

    fix: user repository rules, find by username, not email

--- backend/src/domain/repositories/user.repository.ts
+++ backend/src/domain/repositories/user.repository.ts
@@ -2,6 +2,5 @@ import { UserModel } from "../models/user.model";
 import { IBaseRepository } from "./base.repository";
 
 export interface IUsersRepository extends IBaseRepository<UserModel> {
-    existsByEmail(email: string): Promise<boolean>;
-    findOneByEmail(email: string): Promise<UserModel | null>;
+    findOneByUsername(username: string): Promise<UserModel | null>;
 }
