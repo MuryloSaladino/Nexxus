dcdbdc9b85222c8ca840660a5122e12f2cf55773
MuryloSaladino <murylosaladino@gmail.com>
2025-02-25 21:08:34 -0300

    feat: app client

---
+++ frontend/src/service/internal.services.ts
@@ -0,0 +1,10 @@
+import { baseGet, baseDelete, basePatch, basePost, basePut } from ".";
+import { Connections } from "../constants/connections";
+
+export default {
+    get:    <R = any>(url: string) => baseGet<R>(Connections.InternalAPI + url),
+    post:   <R = any, P = any>(url: string, payload?: P) => basePost<R>(Connections.InternalAPI + url, payload),
+    patch:  <R = any, P = any>(url: string, payload?: P) => basePatch<R>(Connections.InternalAPI + url, payload),
+    put:    <R = any, P = any>(url: string, payload?: P) => basePut<R>(Connections.InternalAPI + url, payload),
+    delete: <R = any>(url: string) => baseDelete<R>(Connections.InternalAPI + url),
+}
